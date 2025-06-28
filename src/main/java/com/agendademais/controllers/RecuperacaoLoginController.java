0fb9e44d9a684ecf598ec153c263ef434991aa20
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-06-27 23:05:49 -0300

    feat: ajustes na view login com enivo SimpleMailMessage

--- src/main/java/com/agendademais/controllers/RecuperacaoLoginController.java
+++ src/main/java/com/agendademais/controllers/RecuperacaoLoginController.java
@@ -1,8 +1,11 @@
 package com.agendademais.controllers;
 
 import com.agendademais.entities.Usuario;
+import com.agendademais.repositories.InstituicaoRepository;
 import com.agendademais.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +18,35 @@ public class RecuperacaoLoginController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    
+    @Autowired
+    private InstituicaoRepository instituicaoRepository;
+    
+    @Autowired
+    private JavaMailSender mailSender;
+
 
     @PostMapping("/recuperar-login")
-    public String recuperarLogin(@RequestParam("emailRecuperacao") String email, Model model) {
+    public String recuperarLogin(@RequestParam String email, Model model) {
         Optional<Usuario> usuarioOpt = usuarioRepository.findByEmailPessoa(email);
 
-        
         if (usuarioOpt.isPresent()) {
-            Usuario usuario = usuarioOpt.get();
-            model.addAttribute("mensagem", "Seu código de usuário é: " + usuario.getCodUsuario() + " e sua senha é: " + usuario.getSenha());
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setSubject("Recuperação de Usuário/Senha");
+            message.setText("Olá!\n\nSeu usuário é: " + usuarioOpt.get().getCodUsuario());
+            mailSender.send(message);
+
+            model.addAttribute("mensagemRecuperacao",
+                    "Enviamos uma mensagem para o e-mail " + email);
         } else {
-            model.addAttribute("mensagem", "Email não encontrado. Verifique se digitou corretamente ou entre em contato com o administrador.");
+            model.addAttribute("mensagemRecuperacao",
+                    "Email não cadastrado.");
         }
+        
+        model.addAttribute("instituicoes", instituicaoRepository.findAll());
         return "login";
     }
+
+
 }
