f6dd1eeaf659c4e234c1db0d99b568f17b0310c3
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-07-18 23:00:55 -0300

    feat: implementar quebra da mensagem de sucesso com  <br>

--- src/main/java/com/agendademais/controllers/ExcluirVinculosInstituicaoController.java
+++ src/main/java/com/agendademais/controllers/ExcluirVinculosInstituicaoController.java
@@ -28,19 +28,19 @@ public class ExcluirVinculosInstituicaoController {
     private PessoaSubInstituicaoRepository pessoaSubInstituicaoRepository;
 
     @Autowired
-    private UsuarioRepository usuarioRepository;    
-    
+    private UsuarioRepository usuarioRepository;
+
     @Autowired
     private PessoaRepository pessoaRepository;
-    
+
     @Transactional
     @PostMapping("/excluir-vinculos-instituicao")
     public String excluirVinculos(HttpSession session, RedirectAttributes redirectAttributes) {
 
         System.out.println("*** ");
-    	System.out.println("*** 1. ExcluirVinculosInstituicaoController ***********************************");
+        System.out.println("*** 1. ExcluirVinculosInstituicaoController ***********************************");
         System.out.println("*** ");
-    	
+
         Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
         Instituicao instituicao = (Instituicao) session.getAttribute("instituicaoSelecionada");
 
@@ -48,58 +48,58 @@ public class ExcluirVinculosInstituicaoController {
             redirectAttributes.addFlashAttribute("mensagemErro", "Sessão inválida. Faça login novamente.");
             return "redirect:/login";
         }
-        
+
         Pessoa pessoa = usuario.getPessoa();
         Long instituicaoId = (Long) instituicao.getId();
-        
-     	System.out.println("*** 2. ExcluirVinculosInstituicaoController ***********************************");
-     	System.out.println("***    usuario                     = " + usuario); 
-     	System.out.println("***    usuario.getSituacaoUsuario()= " + usuario.getSituacaoUsuario()); 
-     	System.out.println("***    usuario.getPessoa()         = " + usuario.getPessoa()); 
-     	System.out.println("***    pessoa.getId()              = " + pessoa.getId()); 
-     	System.out.println("***    instituicao                 = " + instituicao); 
-     	System.out.println("***    instituicaoId               = " + instituicaoId); 
-     	System.out.println("****************************************************************************");
+
+        System.out.println("*** 2. ExcluirVinculosInstituicaoController ***********************************");
+        System.out.println("***    usuario                     = " + usuario);
+        System.out.println("***    usuario.getSituacaoUsuario()= " + usuario.getSituacaoUsuario());
+        System.out.println("***    usuario.getPessoa()         = " + usuario.getPessoa());
+        System.out.println("***    pessoa.getId()              = " + pessoa.getId());
+        System.out.println("***    instituicao                 = " + instituicao);
+        System.out.println("***    instituicaoId               = " + instituicaoId);
+        System.out.println("****************************************************************************");
         System.out.println("*** ");
-        System.out.println("***    Antes delete - usuarioInstituicaoRepository.deleteByUsuarioAndInstituicao(usuario, instituicao");
-        
-     // Exclui vínculos com a instituição atual
+        System.out.println(
+                "***    Antes delete - usuarioInstituicaoRepository.deleteByUsuarioAndInstituicao(usuario, instituicao");
+
+        // Exclui vínculos com a instituição atual
         usuarioInstituicaoRepository.deleteByUsuarioAndInstituicao(usuario, instituicao);
         inscricaoRepository.deleteByIdPessoaAndIdInstituicao(pessoa, instituicao);
         pessoaInstituicaoRepository.deleteByPessoaAndInstituicao(pessoa, instituicao);
         pessoaSubInstituicaoRepository.deleteByPessoaAndInstituicao(pessoa, instituicao);
-        
+
         System.out.println("*** ");
-     	System.out.println("*** 3. Relacionamento com a Instituição excluído com sucesso."); 
-     	System.out.println("****************************************************************************");
+        System.out.println("*** 3. Relacionamento com a Instituição excluído com sucesso.");
+        System.out.println("****************************************************************************");
 
         // 2. Verifica se o usuário ainda tem vínculos com outras instituições
         List<UsuarioInstituicao> outrosVinculos = usuarioInstituicaoRepository.findByUsuario(usuario);
 
         if (outrosVinculos.isEmpty()) {
             // Exclui todas as inscrições vinculadas à pessoa
-               inscricaoRepository.deleteByIdPessoa(pessoa);
+            inscricaoRepository.deleteByIdPessoa(pessoa);
 
             // Exclui usuário e pessoa
             usuarioRepository.delete(usuario);
             pessoaRepository.delete(pessoa);
             redirectAttributes.addFlashAttribute(
-               "mensagemSucesso",
-               "Relacionamento com a Instituição excluído com sucesso.\n"
-               + "Nenhum outro vínculo restante. Cadastro geral removido.\n"
-               + usuario.getCodUsuario() + " - " + pessoa.getNomePessoa()
-            );
-            
+                    "mensagemSucesso",
+                    "Relacionamento com a Instituição excluído com sucesso.<br>"
+                            + "Nenhum outro vínculo restante. Cadastro geral removido.<br>"
+                            + usuario.getCodUsuario() + " - " + pessoa.getNomePessoa());
+
         } else {
             redirectAttributes.addFlashAttribute(
-            	"mensagemSucesso", 
-            	"Relacionamento com a Instituição excluído com sucesso: \n"
-                + usuario.getCodUsuario() + " - " + pessoa.getNomePessoa() + "\n"  
-                + instituicao.getNomeInstituicao());
-        }     	
+                    "mensagemSucesso",
+                    "Relacionamento com a Instituição excluído com sucesso:<br>"
+                            + usuario.getCodUsuario() + " - " + pessoa.getNomePessoa() + "<br>"
+                            + instituicao.getNomeInstituicao());
+        }
 
         session.invalidate();
         return "redirect:/login";
-        
+
     }
 }
